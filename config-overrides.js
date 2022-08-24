e2726227a6c2458fce6e6e9603fca097ef856cba
xzx <1015776502@qq.com>
2022-08-24 23:49:04 +0800

    添加

---
+++ config-overrides.js
@@ -0,0 +1,31 @@
+const {
+  override,
+  addWebpackAlias,
+  addWebpackModuleRule
+} = require('customize-cra')
+const path = require('path')
+module.exports = override(
+  (config) => {
+    config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false
+    return config
+  },
+  // @别名
+  addWebpackAlias({
+    '@': path.resolve('./src')
+  }),
+  // scss全局变量
+  addWebpackModuleRule({
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'sass-loader',
+      {
+        loader: 'sass-resources-loader',
+        options: {
+          resources: ['./src/style/basics.scss']
+        }
+      }
+    ]
+  })
+)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '@/assets/logo.svg'
+import './App.scss'
+function App () {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  )
+}
+
+export default App
