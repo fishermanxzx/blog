537ea141b5845a67ab95d49e3cc0ae8ca57653b2
xzx <1015776502@qq.com>
2023-02-01 17:12:04 +0800

    添加返回和回到顶部按钮

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,8 @@ import beian from '@/assets/beian.png'
 import './App.scss'
 import routes from './routes'
 import { useRoutes, useLocation } from 'react-router-dom'
+import BackToTop from '@/components/BackToTop'
+import BackToHome from '@/components/BackToHome'
 function App() {
   const element = useRoutes(routes)
   const location = useLocation()
@@ -17,6 +19,8 @@ function App() {
         <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44060502003107" rel="noreferrer">粤公网安备 44060502003107号</a>&emsp;
         <a href="https://beian.miit.gov.cn" target="_blank" rel="noreferrer">粤ICP备2022109946号-1</a>
       </footer>
+      <BackToTop></BackToTop>
+      <BackToHome></BackToHome>
     </div>
   )
 }
---
+++ app/src/components/BackToTop/index.tsx
@@ -0,0 +1,16 @@
+import Icon from '@/Icon'
+import React, { useCallback } from 'react'
+import './index.scss'
+
+export default function BackToTop() {
+  const toTop = useCallback(() => {
+    document.documentElement.style.scrollBehavior = 'smooth'
+    document.documentElement.scrollTop = 0
+    document.documentElement.style.scrollBehavior = 'auto'
+  }, [])
+  return (
+    <div className='BackToTop' onClick={toTop}>
+        <Icon icon='arrow-up' className='icon'></Icon>
+    </div>
+  )
+}
